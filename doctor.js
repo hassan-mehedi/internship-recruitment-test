28fd9c3d35f4daaa34ade087bd219789414c60c9
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2021-09-17 21:48:24 +0600

    add doctor functionality added

--- doctor.js
+++ doctor.js
@@ -7,7 +7,7 @@ const doctors = [
         doctorType: "Consultant",
         email: "jabub@hospital.com",
         active: true,
-        doctorRequests: []
+        doctorRequests: [],
     },
     {
         doctorID: "6216",
@@ -16,7 +16,7 @@ const doctors = [
         teamID: "008",
         doctorType: "Assistant Consultant",
         email: "amhed@hospital.com",
-        active: true
+        active: true,
     },
     {
         doctorID: "6214",
@@ -26,7 +26,7 @@ const doctors = [
         doctorType: "Consultant",
         email: "malim@hospital.com",
         active: true,
-        doctorRequests: ["6213", ]
+        doctorRequests: ["6213"],
     },
     {
         doctorID: "6213",
@@ -35,22 +35,56 @@ const doctors = [
         teamID: "005",
         doctorType: "RMO",
         email: "jagum@hospital.com",
-        active: false
+        active: false,
     },
-
-]
+];
 
 const team = [
     {
         teamName: "nephrology",
         teamID: "008",
         consultantInCharge: "6215",
-        teamMates: ["6216",]
+        teamMates: ["6216"],
     },
     {
         teamName: "cardiology",
         teamID: "005",
         consultantInCharge: "6214",
-        teamMates: []
+        teamMates: [],
     },
-]
+];
+
+const getThelastKey = () => {
+    doctors.sort((a, b) => {
+        return a.doctorID - b.doctorID;
+    });
+
+    return doctors[doctors.length - 1].doctorID;
+};
+
+const makeMail = (firstName, lastName) => {
+    let mail = firstName.slice(0, 2) + lastName.slice(-3) + "@hospital.com";
+    return mail;
+};
+
+const newDoctor = (firstName, lastName, doctorType) => {
+    let lastID = getThelastKey();
+    let nextID = (+lastID + 1).toString();
+    let mail = makeMail(firstName, lastName);
+    const doctor = {
+        doctorID:
+            nextID.length === 1
+                ? "00" + nextID
+                : nextID.length === 2
+                ? "0" + nextID
+                : nextID,
+        firstName,
+        lastName,
+        doctorType,
+        email: mail,
+        active: false,
+    };
+    doctors.push(doctor);
+};
+// Adding a new doctor
+newDoctor("Mehedi", "Hassan", "RMO");
